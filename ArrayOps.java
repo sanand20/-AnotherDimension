a8f6a762fd6f6357c25c171e99882fb0b3828d3a
Shriya <sanand20@stuy.edu>
2020-10-26 18:12:45 -0400

    sumrows done

---
+++ ArrayOps.java
@@ -0,0 +1,26 @@
+public class ArrayOps {
+    public static int sum(int[] arr){
+    int ans =0;
+    for (int i=0; i<arr.length; i++){
+      ans+= arr[i];
+    }
+    return ans;
+  }
+    public static int largest(int [] arr){
+      int ans=0;
+      for (int i = 0; i<arr.length; i++){
+        if (arr[i]>ans){
+          ans= arr[i];
+        }
+      }
+      return ans;
+    }
+    public static int[] sumRows(int[][] matrix){
+      int [] ans= new int [matrix.length];
+      for (int i=0; i<matrix.length; i++){
+        ans[i]= sum(matrix[i]);
+      }
+      return ans;
+    }
+
+}
