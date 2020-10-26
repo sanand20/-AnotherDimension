ea6361103d2e70b4889f996e5fb9ee757ec59390
Shriya <sanand20@stuy.edu>
2020-10-26 18:08:51 -0400

    largest done

--- Tester.java
+++ Tester.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class Tester{
   public static void main(String[]args){
 
@@ -6,6 +7,10 @@ public class Tester{
     arrays[1]= new int [] {6,3,100};
     arrays[2]= new int [] {9,2,10};
 
+    int [][][]array = new int[2][][];
+    array[0]= new int [][] {{9,2,1},{3,4,5,6}};
+    array[1]= new int [][] { {1,0,12,-1},{7,-2,2,1},{ -5, -2,  2, -9 }};
+
 
     System.out.println(ArrayOps.sum(arrays[0]));
     System.out.println(ArrayOps.sum(arrays[1]));
@@ -13,6 +18,9 @@ public class Tester{
     System.out.println(ArrayOps.largest(arrays[0]));
     System.out.println(ArrayOps.largest(arrays[1]));
     System.out.println(ArrayOps.largest(arrays[2]));
+    System.out.println(Arrays.toString(ArrayOps.sumRows(array[0])));
+    System.out.println(ArrayOps.sum(array[1]));
+
 
   }
 }
